41e116622bc022316ef24cc5917d104628290b9e
gautambedi1998 <41571537+gautambedi1998@users.noreply.github.com>
2018-07-23 21:42:18 +1000

    Add files via upload

---
+++ sos.ino
@@ -0,0 +1,41 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() { 
+  //Morse code for S
+  // put your main code to run repeatedly
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(500);                      
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000); 
+    digitalWrite(LED_BUILTIN, HIGH);  
+  delay(500);                      
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000); 
+  //Morse code for o
+digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);  
+  //Morse code for s
+    digitalWrite(LED_BUILTIN, HIGH);  
+  delay(500);                      
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000); 
+    digitalWrite(LED_BUILTIN, HIGH);  
+  delay(500);                      
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+}
+  
+                     
+                        
+
